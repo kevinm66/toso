7db89ce2d5a154a5d26ff201082e438b8c515815
stano <stano@DESKTOP-98MHEH2>
2017-07-13 10:22:47 +0200

    pridanie dalsieho modulu pre commercial testing

---
+++ Toso/src/main/java/com/toso/commercialtesting/domain/TestC.java
@@ -0,0 +1,17 @@
+package com.toso.commercialtesting.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class TestC {
+	int testDuration;
+	int numberOfQuestions;
+	String name;
+	
+	
+	
+
+}
